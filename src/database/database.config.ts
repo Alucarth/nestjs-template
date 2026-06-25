3f79885aefd60aa2931368d8eac971ab27f4f71c
alucarth <ltorrezs2008@gmail.com>
2026-06-25 18:09:19 -0400

    database connet

---
+++ src/database/database.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const databaseConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASS'),
+  database: configService.get<string>('DB_NAME'),
+  autoLoadEntities: true,
+  synchronize: configService.get<boolean>('DB_SYNC'), // colocar false en produccion
+});
