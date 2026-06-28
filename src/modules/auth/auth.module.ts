15e3be26ffda435f4cbc6e495803a3356eac2103
alucarth <ltorrezs2008@gmail.com>
2026-06-28 12:58:41 -0400

    remove constant and enable jwt for enviroment

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,16 +5,20 @@ import { UserModule } from '../user/user.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants/jwt.constant';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: `${configService.get('JWT_TTL')}` },
+      }),
     }),
   ],
   controllers: [AuthController],
--- src/modules/auth/constants/jwt.constant.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-  secret: 'THE GRANT MOTHERS NAME IS FLORENCIA.',
-};
